29258dbe6f07201a748d57e035d5a8be31521407
Ruben Vazquez-Chapa <ravc@protonmail.com>
2018-01-25 16:15:43 -0600

    Initial commit

---
+++ network_scanner.py
@@ -0,0 +1,43 @@
+import os
+import time
+from definitions import TXT
+
+whitelist = []
+
+logged = []
+
+IPS = input('Input start IP address: ')
+IPE = input('Input end IP address: ')
+Loop = input('Loop the scan? ')
+IP = IPS.split('.')
+IP2 = IPE.split('.')
+IP3 = IP[-1]
+
+if Loop == 'y':
+    while True:
+        for i in range(int(IP[-1]), int(IP2[-1])+1):
+            IP = '.'.join(IP)
+            response = os.system('ping -c 1 ' + IP + ' > /dev/null')
+            if response == 0:
+                print(IP + ' is up')
+                TXT('nscan.txt', IP + ' is up')
+            else:
+                print(IP + ' is down')
+                TXT('nscan.txt', IP + ' is down')
+            IP = IP.split('.')
+            IP[-1] = str(int(IP[-1]) + 1)
+        IP[-1] = IP3
+elif Loop == 'n':
+    for i in range(int(IP[-1]), int(IP2[-1])+1):
+        IP = '.'.join(IP)
+        response = os.system('ping -c 1 ' + IP + ' > /dev/null')
+        if response == 0:
+            print(IP + ' is up')
+            TXT('nscan.txt', IP + ' is up')
+        else:
+            print(IP + ' is down')
+            TXT('nscan.txt', IP + ' is down')
+        IP = IP.split('.')
+        IP[-1] = str(int(IP[-1]) + 1)
+else:
+    print('Not a valid input')
